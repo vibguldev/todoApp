a3f2174a15664cd7810178c69ab95c961673a78d
vibguldev <gulativibhav@gmail.com>
2017-02-14 18:14:27 +0530

    styling complete

--- public/script.js
+++ public/script.js
@@ -1,84 +1,80 @@
 let filterModeFlag
 function htmlStringify(obj) {
-  // if (obj.status === true) {
-    if (obj) {
-  let checked = obj.status === true ? 'checked' : null
-  var string =
-    `<li>
-    <input class="toggle" type="checkbox" onclick="edit(this)" ${checked} id=status${obj.id}>
-    <input class="todo-text-${checked ? 'striked' : ''}"  type="text" value="${obj.description}" id=text${obj.id} readonly="true" ondblclick="enableText(this)" onfocusout="edit(this)" autocomplete="off">
-    <button class="delete" onclick="destroy(this)" id=${obj.id}>X</button>
-    </li>`
-    document.getElementById("abc").style.display = (todos.length > 0) ? 'block' : 'none'
-  return string
-    }
-    else {
-      document.getElementById("abc").style.display = (todos.length > 0) ? 'block' : 'none'
-    }
+  if (obj) {
+    let checked = obj.status === true ? 'checked' : null
+    var string =
+      `<li>
+      <input class="toggle" type="checkbox" onclick="edit(this)" ${checked} id=status${obj.id}>
+      <input class="todo-text ${checked ? 'striked' : ''}"  type="text" value="${obj.description}" id=text${obj.id} readonly="true" ondblclick="enableText(this)" onfocusout="edit(this)" autocomplete="off">
+      <button class="delete" onclick="destroy(this)" id=${obj.id}>X</button>
+      </li>`
+    checkFooterVisibility()
+    return string
+  }
+  else {
+    checkFooterVisibility()
+  }
 }
 
-function enableText(obj) {
-  obj.readOnly = ""
+function checkFooterVisibility () {
+  document.getElementById('filter-footer').style.display = (todos.length > 0) ? 'block' : 'none'
 }
 
-function checkFilterModeFlag() {
-  // document.getElementsByClassName("footer").style.visibility = todos.length > 0 ? 'visible' : 'hidden'
-  console.log("flag : ",filterModeFlag)
+function enableText (obj) {
+  obj.readOnly = ''
+  const deleteButtonID = obj.id.slice(4)
+  const checkBoxID = 'status' + obj.id.slice(4)
+  document.getElementById(obj.id).className = 'edit'
+  document.getElementById(deleteButtonID).style.visibility = 'hidden'
+  document.getElementById(checkBoxID).style.visibility = 'hidden'
+}
+
+function checkFilterModeFlag () {
   if (filterModeFlag === 0) {
     filterTodos('all')
-  }
-  else if (filterModeFlag === 1) {
+  } else if (filterModeFlag === 1) {
     filterTodos('active')
-  }
-  else if (filterModeFlag === 2) {
+  } else if (filterModeFlag === 2) {
     filterTodos('completed')
   }
 }
 
-
-
-function checkAll() {
-  
+function checkAll () {
   if (todos.every((todo) => (todo.status === true))) {
     fetch(`/unCheckAll`, {
-      method: "PUT"
+      method: 'PUT'
     }).then((response) => {
       console.log(response)
-      document.getElementById("this").innerHTML = null
+      document.getElementById('this').innerHTML = null
       todos.forEach((todo) => {
         todo.status = false
-        // document.getElementById("this").innerHTML += htmlStringify(todo)
-
       })
-       checkFilterModeFlag()
-      document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
-
+      checkFilterModeFlag()
+      document.getElementById('number-of-todos').innerHTML = todos.filter((todo) => (todo.status === false)).length
     })
-  }
-  else {
+  } else {
     fetch(`/checkAll`, {
-      method: "PUT"
+      method: 'PUT'
     }).then((response) => {
       console.log(response)
-      document.getElementById("this").innerHTML = null
+      document.getElementById('this').innerHTML = null
       todos.forEach((todo) => {
         todo.status = true
         // document.getElementById("this").innerHTML += htmlStringify(todo)
       })
       checkFilterModeFlag()
-      document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
-
+      document.getElementById('number-of-todos').innerHTML = todos.filter((todo) => (todo.status === false)).length
     })
   }
 }
 
 // <button class="edit" onclick="edit(this)" id=${obj.id}>Edit</button>
 // <label class="${checked ? 'striked' : ''}">"${obj.description} "</label>
-document.getElementById("enterToDo").onkeydown = function (event) {
+document.getElementById('enterToDo').onkeydown = function (event) {
   if (event.keyCode === 13) {
-    var todo = document.getElementById("enterToDo").value
+    var todo = document.getElementById('enterToDo').value
     fetch(`/write/${todo}`, {
-      method: "POST"
+      method: 'POST'
     }).then((response) => {
       response.json()
         .then((json) => {
@@ -89,53 +85,36 @@ document.getElementById("enterToDo").onkeydown = function (event) {
             status: false,
             description: todo
           }
-          // document.getElementById("this").innerHTML += htmlStringify(todos[length])
           checkFilterModeFlag()
-          document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
-
+          document.getElementById('number-of-todos').innerHTML = todos.filter((todo) => (todo.status === false)).length
         })
         .catch((response) => {
           console.log(response)
         })
-      console.log(response)
-      // read()
     })
       .catch((response) => {
         console.log(response)
-      });
-    document.getElementById("enterToDo").value = null
-    // let row = ''
-    // todos.forEach((todo) => {
-    //   row = row + htmlStringify(todo)
-    // })
-
+      })
+    document.getElementById('enterToDo').value = null
   }
 }
-function edit(element) {
+function edit (element) {
   var data
   var statusFlag = false
   var descriptionFlag = false
   if (element.id.slice(0, 6) === 'status') {
     var todoId = element.id.slice(6)
     var todoStatus = document.getElementById(`status${todoId}`).checked.toString()
-    data = {
-      status: todoStatus
-    }
+    data = {status: todoStatus}
     statusFlag = true
-  }
-  else {
+  } else {
     var todoId = element.id.slice(4)
     var updateToDoText = document.getElementById(`text${todoId}`).value
-    data = {
-      description: updateToDoText
-    }
+    data = {description: updateToDoText}
     descriptionFlag = true
   }
-  // console.log(updateToDoText)
-
-
   fetch(`/update/${todoId}`, {
-    method: "PUT",
+    method: 'PUT',
     body: JSON.stringify(data),
     headers: {
       'Content-Type': 'application/json'
@@ -145,27 +124,25 @@ function edit(element) {
     let index = todos.findIndex(x => x.id === todoId)
     if (descriptionFlag) {
       todos[index].description = updateToDoText
-    }
-    else if (statusFlag) {
+    } else if (statusFlag) {
       todoStatus = todoStatus === 'true'
       todos[index].status = todoStatus
     }
-    document.getElementById("this").innerHTML = null
+    document.getElementById('this').innerHTML = null
     // todos.forEach((todo) => {
     //   document.getElementById("this").innerHTML += htmlStringify(todo)
     // })
     checkFilterModeFlag()
-    document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
-
+    document.getElementById('number-of-todos').innerHTML = todos.filter((todo) => (todo.status === false)).length
   })
     .catch((response) => {
       console.log(response)
     })
 }
 
-function filterTodos(filterMode) {
-  document.getElementById(filterMode).className = "selected-filters"
-  document.getElementById("this").innerHTML = null
+function filterTodos (filterMode) {
+  document.getElementById(filterMode).className = 'selected-filters'
+  document.getElementById('this').innerHTML = null
   switch (filterMode) {
     case 'all': {
       filterModeFlag = 0
@@ -199,27 +176,20 @@ function filterTodos(filterMode) {
   }
 }
 
-////////resolve this function >>>>>>>>><<<<<<<<<>>>>>>><<<#$^#^#^#%#%&#%#%^#^#$^#$%@#%@%#$&%^*%*^&*%^&$%&#^#$^#^$#&$^&%^&*$&$&#%^#$^@$%@$%@$%
 function destroyAllChecked() {
   fetch(`/destroyAllChecked`, {
     method: "DELETE"
   }).then((response) => {
     console.log("response:.........", response)
     document.getElementById("this").innerHTML = null
-    var newTodos = []
+    var newTodos = []  //a dummy array created to avoid the issue of splice
     todos.forEach((todo, index) => {
       if (todo.status === false) {
-        // todos.splice(index, 1)
         newTodos.push(todo)
-        console.log("here")
       }
     })
     todos = newTodos
-      // console.log(todos)
-      // todos.forEach ((todo) => {
-      //   document.getElementById("this").innerHTML += htmlStringify(todo)
-      // })
-      document.getElementById("abc").style.display = (todos.length > 0) ? 'block' : 'none'
+      checkFooterVisibility()
       checkFilterModeFlag()
       document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
     })
@@ -239,7 +209,7 @@ function destroy(buttonElement) {
     let index = todos.findIndex(x => x.id === buttonClickedId)
     todos.splice(index, 1)
     document.getElementById("this").innerHTML = null
-    document.getElementById("abc").style.display = (todos.length > 0) ? 'block' : 'none'
+    checkFooterVisibility()
     todos.forEach((todo) => {
       document.getElementById("this").innerHTML += htmlStringify(todo)
     })
@@ -248,8 +218,6 @@ function destroy(buttonElement) {
     .catch((response) => {
       console.log(response)
     })
-
-
 }
 
 var todos = []
@@ -277,16 +245,12 @@ const read = () => {
         })
         document.getElementById("this").innerHTML = row;
         document.getElementById("number-of-todos").innerHTML = todos.filter((todo) => (todo.status === false)).length
-
-
       })
   .then((response) => {
     console.log("inner catch")
     console.log(todos.length)
     htmlStringify()
-
   })
-
   }).catch((err) => {
     console.log("outer catch")
   })
